be770a21dae415f58228e2b88a84cc0fb799d72a
Wild Tan <61940455+GoogeTan@users.noreply.github.com>
2020-10-25 21:44:49 +0300

    114

--- src/main/java/phoenix/client/gui/diaryPages/elements/ADiaryChapter.java
+++ src/main/java/phoenix/client/gui/diaryPages/elements/ADiaryChapter.java
@@ -2,6 +2,7 @@ package phoenix.client.gui.diaryPages.elements;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
+import phoenix.utils.ArrayUtils;
 
 import java.util.ArrayList;
 
@@ -9,32 +10,86 @@ public abstract class ADiaryChapter
 {
     FontRenderer font;
     ArrayList<IDiaryElement> elements;
-    private int start = 0, end = 14;
+    private int start = 0, end = 14, end2 = 28;
     public ADiaryChapter()
     {
         font = Minecraft.getInstance().fontRenderer;
     }
 
 
-    public void add(ArrayList<IDiaryElement> elements)
+    public void add(ArrayList<IDiaryElement> elementsIn)
     {
-        elements.addAll(elements);
+        elements.addAll(elementsIn);
+        recalculateSizes();
+    }
+    private void recalculateSizes()
+    {
         int size = 0;
+        int count  = 0;
+        int count1 = 0;
         for (IDiaryElement element : elements)
         {
-            if (size + element.getHeight() >= 14)
+            if(size + element.getHeight() >= 28)
             {
-                start = 0;
-                end = size;
-                return;
+                end2 += count;
+            }
+            else if (size + element.getHeight() >= 14)
+            {
+                start += 0;
+                end   += count;
             }
             size += element.getHeight();
+            if(count == count1)
+                count++;
+
+            count1++;
         }
     }
-
-    public ArrayList<IDiaryElement> getCurrectPages()
+    public ArrayList<IDiaryElement> getCurrentPage1()
     {
-
+        return ArrayUtils.part(elements, start, end);
     }
-    abstract ArrayList<IDiaryElement> next();
+
+    public ArrayList<IDiaryElement> getCurrentPage2()
+    {
+        return ArrayUtils.part(elements, end, end2);
+    }
+
+    public void next()
+    {
+        int size  = 0;
+        int count = 0;
+        for (int i = end; i < elements.size(); i++)
+        {
+            if (size + elements.get(i).getHeight() >= 14)
+            {
+                start += count + 1;
+                end   += count + 1;
+                return;
+            }
+            size += elements.get(i).getHeight();
+            count++;
+        }
+        start += count + 1;
+        end   += count + 1;
+    }
+
+    public void prev()
+    {
+        int size  = 0;
+        int count = 0;
+        for (int i = start - 1; i >= 0; --i)
+        {
+            if (size + elements.get(i).getHeight() >= 14)
+            {
+                start -= count + 1;
+                end   -= count + 1;
+                return;
+            }
+            size += elements.get(i).getHeight();
+            count++;
+        }
+        start -= count + 1;
+        end   -= count + 1;
+    }
 }
--- src/main/java/phoenix/utils/ArrayUtils.java
+++ src/main/java/phoenix/utils/ArrayUtils.java
@@ -21,4 +21,22 @@ public class ArrayUtils
         res.addAll(Arrays.asList(adv));
         return res;
     }
+
+    public static <T> ArrayList<T> part(ArrayList<T> list, int from, int to)
+    {
+        ArrayList<T> res = new ArrayList<>();
+        for (int i = from; i < to; i++)
+        {
+            res.add(list.get(getIndex(list.size(), i)));
+        }
+        return res;
+    }
+
+    private static int getIndex(int size, int index)
+    {
+        while (index < 0)
+            index += size;
+        //index %= size;
+        return index;
+    }
 }
