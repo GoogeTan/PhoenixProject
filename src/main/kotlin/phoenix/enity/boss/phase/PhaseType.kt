4865bdb9cd45f5df9376250b40c007a45dac7e5b
Wild Tan <61940455+GoogeTan@users.noreply.github.com>
2021-04-15 09:42:58 +0300

    a

--- src/main/kotlin/phoenix/enity/boss/phase/PhaseType.kt
+++ src/main/kotlin/phoenix/enity/boss/phase/PhaseType.kt
@@ -6,7 +6,12 @@ import phoenix.enity.boss.phase.phases.redo.*
 import phoenix.utils.ArrayUtils.resize
 import phoenix.world.StageManager
 
-class PhaseType private constructor(private val id: Int, private val constructor: (AbstractEnderDragonEntity) -> IPhase, private val name: String)
+class PhaseType private constructor
+    (
+        private val id: Int,
+        private val constructor: (AbstractEnderDragonEntity) -> IPhase,
+        private val name: String
+    )
 {
     fun createPhase(dragon: AbstractEnderDragonEntity): IPhase = constructor(dragon)
 
@@ -17,29 +22,30 @@ class PhaseType private constructor(private val id: Int, private val constructor
     companion object
     {
         private var phases = ArrayList<PhaseType?>()
-        val HOLDING_PATTERN = create(::HoldingPatternPhase, "HoldingPattern")
-        val STRAFE_PLAYER = create(::StrafePlayerPhase, "StrafePlayer")
-        val LANDING_APPROACH = create(::LandingApproachPhase, "LandingApproach")
-        val LANDING = create(::LandingPhase, "Landing")
-        val TAKEOFF = create(::TakeoffPhase, "Takeoff")
-        val SITTING_FLAMING = create(::FlamingSittingPhase, "SittingFlaming")
-        val SITTING_SCANNING = create(::ScanningSittingPhase, "SittingScanning")
-        val SITTING_ATTACKING = create(::AttackingSittingPhase, "SittingAttacking")
-        val CHARGING_PLAYER = create(::ChargingPlayerPhase, "ChargingPlayer")
-        val DYING = create(::DyingPhase, "Dying")
-        val HOVER = create(::HoverPhase, "Hover")
 
-        val REDO_HOLDING_PATTERN = create(::RedoHoldingPatternPhase, "RedoHoldingPattern")
-        val REDO_STRAFE_PLAYER = create(::RedoStrafePlayerPhase, "RedoStrafePlayer")
-        val REDO_LANDING_APPROACH = create(::RedoLandingApproachPhase, "LandingApproach")
-        val REDO_LANDING = create(::RedoLandingPhase, "Landing")
-        val REDO_TAKEOFF = create(::RedoTakeoffPhase, "RedoTakeoff")
-        val REDO_SITTING_FLAMING = create(::RedoFlamingSittingPhase, "SittingFlaming")
-        val REDO_SITTING_SCANNING = create(::RedoScanningSittingPhase, "SittingScanning")
+        val HOLDING_PATTERN   = create(::HoldingPatternPhase, "HoldingPattern")
+        val STRAFE_PLAYER     = create(::StrafePlayerPhase, "StrafePlayer")
+        val LANDING_APPROACH  = create(::LandingApproachPhase, "LandingApproach")
+        val LANDING           = create(::LandingPhase, "Landing")
+        val TAKEOFF           = create(::TakeoffPhase, "Takeoff")
+        val SITTING_FLAMING   = create(::FlamingSittingPhase, "SittingFlaming")
+        val SITTING_SCANNING  = create(::ScanningSittingPhase, "SittingScanning")
+        val SITTING_ATTACKING = create(::AttackingSittingPhase, "SittingAttacking")
+        val CHARGING_PLAYER   = create(::ChargingPlayerPhase, "ChargingPlayer")
+        val DYING             = create(::DyingPhase, "Dying")
+        val HOVER             = create(::HoverPhase, "Hover")
+
+        val REDO_HOLDING_PATTERN   = create(::RedoHoldingPatternPhase, "RedoHoldingPattern")
+        val REDO_STRAFE_PLAYER     = create(::RedoStrafePlayerPhase, "RedoStrafePlayer")
+        val REDO_LANDING_APPROACH  = create(::RedoLandingApproachPhase, "LandingApproach")
+        val REDO_LANDING           = create(::RedoLandingPhase, "Landing")
+        val REDO_TAKEOFF           = create(::RedoTakeoffPhase, "RedoTakeoff")
+        val REDO_SITTING_FLAMING   = create(::RedoFlamingSittingPhase, "SittingFlaming")
+        val REDO_SITTING_SCANNING  = create(::RedoScanningSittingPhase, "SittingScanning")
         val REDO_SITTING_ATTACKING = create(::RedoAttackingSittingPhase, "SittingAttacking")
-        val REDO_CHARGING_PLAYER = create(::RedoChargingPlayerPhase, "RedoChargingPlayer")
-        val REDO_DYING = create(::RedoDyingPhase, "Dying")
-        val REDO_HOVER = create(::RedoHoverPhase, "Hover")
+        val REDO_CHARGING_PLAYER   = create(::RedoChargingPlayerPhase, "RedoChargingPlayer")
+        val REDO_DYING             = create(::RedoDyingPhase, "Dying")
+        val REDO_HOVER             = create(::RedoHoverPhase, "Hover")
 
         fun getById(idIn: Int) = if (idIn >= 0 && idIn < phases.size) phases[idIn] else StageManager.stageEnum.holdingPhase
 
